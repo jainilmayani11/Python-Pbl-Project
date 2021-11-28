abd86656c6458f7e1982e337f63aa5137d03d21a
Jainil Mayani <86836242+jainilmayani11@users.noreply.github.com>
2021-11-28 16:06:25 +0530

    Add files via upload

---
+++ 21SOECE13043_PBL_PROJECT/Destination/py/sel.py
@@ -0,0 +1,6 @@
+# Module and package
+
+import selenium
+from selenium import webdriver
+web = webdriver.Chrome(executable_path='C://Users//LENOVO//Downloads//chromedriver.exe')
+web.get('https://www.instagram.com/jenu_mayani_11')
